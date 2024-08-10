6ca610fc5ae4813e3cb31df2e34e999849a76bc2
khadim555 <khadimhusainkadiwala3@gmail.com>
2024-08-10 17:21:22 +0530

    xyz

--- if_else.php
+++ if_else.php
@@ -2,14 +2,14 @@
 $age = 50;
 if($age>25)
 {
-     echo "you can drive the car";
+     echo "you can drive the car1";
     
 }
 elseif($age>30)
 {
-    echo "you are retiered ,you cannot drive the car";
+    echo "you are retiered ,you cannot drive the car2";
 }
 else{
-    echo "YOU ARE UNDER AGE";
+    echo "YOU ARE UNDER AGE3";
 }
 ?>
