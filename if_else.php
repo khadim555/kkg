cfaa963c64f7a15f92e806f35c10d9ba73870526
khadim555 <khadimhusainkadiwala3@gmail.com>
2024-08-10 17:15:49 +0530

    index

---
+++ if_else.php
@@ -0,0 +1,15 @@
+<?php
+$age = 50;
+if($age>25)
+{
+     echo "you can drive the car";
+    
+}
+elseif($age>30)
+{
+    echo "you are retiered ,you cannot drive the car";
+}
+else{
+    echo "YOU ARE UNDER AGE";
+}
+?>
